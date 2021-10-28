d8978c8ac7aa1f16a5d5d5a56fcabbde60e2704c
Junoburger <juno.burger@pm.me>
2021-10-28 23:31:48 +0200

    Shorten

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,15 @@
 input_num = abs(int(input("Please enter a positive integer greater than 1: ")))
 
 num1, num2 = 1, 1
-count = 0
+i = 0
 
 if input_num == 1:
     print(num1)
 else:
     print("Fibonacci sequence:")
-    while count < input_num:
+    while i < input_num:
         print(num1)
         nth = num1 + num2
-        # update values
         num1 = num2
         num2 = nth
-        count += 1
+        i += 1
